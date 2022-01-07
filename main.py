ac569b7a0d2c77cde7fb307c39037a7527b98453
McCudles <mccadesmith@outlook.com>
2022-01-07 17:19:33 -0600

    01.07.22

--- main.py
+++ main.py
@@ -5,9 +5,28 @@ import numpy as np
 import controlPoints as ctrlPnts
 import volumeController
 
-
+play = True
 controlPoints = ctrlPnts.points
 startTime = time.time()
+
+
+def run():
+    while True:
+        currentTime = time.time()
+        timeElapsed = currentTime - startTime
+        if timeElapsed >= controlPoints[len(controlPoints) - 1][0]:
+            restartTime()
+        newVolume = utils.getVolume(timeElapsed)
+        volumeController.setVolume(newVolume)
+        time.sleep(0.05)
+
+
+def init():
+    global play
+    restartTime()
+    play = not (play)
+    print(time.time())
+    return print(f"{play}")
 
 
 def restartTime():
@@ -16,12 +35,3 @@ def restartTime():
 
 
 dataPlot = utils.init(controlPoints)
-
-while True:
-    currentTime = time.time()
-    timeElapsed = currentTime - startTime
-    if timeElapsed >= controlPoints[len(controlPoints) - 1][0]:
-        restartTime()
-    newVolume = utils.getVolume(timeElapsed)
-    volumeController.setVolume(newVolume)
-    time.sleep(0.05)
